cd7a408029d1dff68fc8161254b359b98e792a3a
realpj10101 <parsajfr1385oo@gmail.com>
2026-02-19 20:18:26 +0330

    create message response dto

--- backend/api/DTOs/Message.cs
+++ backend/api/DTOs/Message.cs
@@ -3,3 +3,9 @@ namespace api.DTOs;
 public record MessageRequest(
     string Message
 );
+
+public record MessageResponseDto(
+    string Message,
+    string SenderUserName,
+    DateTime TimeStamp
+);
