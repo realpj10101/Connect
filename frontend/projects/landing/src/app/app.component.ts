04514bc6058989d71cd7d00e9f3f0a23251cd5d3
realpj10101 <parsajfr1385oo@gmail.com>
2026-02-21 17:41:45 +0330

    create ui lib and use it in landing

--- frontend/projects/landing/src/app/app.component.ts
+++ frontend/projects/landing/src/app/app.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ThemeService } from '../../../ui/src/lib/services/theme.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'landing';
+export class AppComponent implements OnInit {
+  themeService = inject(ThemeService);
+
+  selectedTheme = '';
+
+  ngOnInit(): void {
+    const theme = this.themeService.getSelectedTheme();
+    this.selectedTheme = theme.name;
+  }
+
+  changeTheme(themeName: string) {
+    this.themeService.setSelectedTheme(themeName);
+    this.selectedTheme = themeName;
+  }
 }
---
+++ frontend/projects/ui/src/lib/services/theme.service.ts
@@ -0,0 +1,24 @@
+import { Injectable, signal } from '@angular/core';
+import { themes } from '../theme';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ThemeService {
+  private _selectedThemeSig = signal(themes[0]);
+
+  getThemes() {
+    return themes;
+  }
+
+  getSelectedTheme() {
+    return this._selectedThemeSig();
+  }
+
+  setSelectedTheme(themeName: string) {
+    const theme = themes.find(t => t.name === themeName);
+    if (theme) {
+      this._selectedThemeSig.set(theme);
+    }
+  }
+}
